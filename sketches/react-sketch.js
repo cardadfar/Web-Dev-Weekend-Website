39f1604c658c49fe82dddc3d75d6f550fdf843f6
LAPTOP-6QBLE82J\Oscar Dadfar <cardadfar@gmail.com>
2018-02-19 10:48:25 -0500

    fourth commit

--- sketches/react-sketch.js
+++ sketches/react-sketch.js
@@ -104,7 +104,10 @@ function draw_edges() {
     var scl = 200;
     var spd = 3000;
     var count = 8;
-    height = 0.91*height;
+    if(width < 400)
+      height = 0.91*height;
+    else
+      height = 0.89*height;
   }
   else {
     var scl = 500;
